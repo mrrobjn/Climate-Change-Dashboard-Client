01fb373700c48b950f7f4b11573808636fca8d87
Le Trung Kien <letrungkien2901@gmail.com>
2023-10-06 21:05:54 +0700

    Add base html frame

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,33 @@ import Banner from '../components/Banner'
 
 const HomePage = () => {
   return (
-    <><Banner/>
+    <>
+      <Banner/>
+      <div>
+        <div>Featured News</div>
+        <div>
+          <div>
+            news feed
+          </div>
+          <div>
+            <div>
+              feed 1
+            </div>
+            <div>
+              feed 2
+            </div>
+            <div>
+              feed 3
+            </div>
+            <div>
+              feed 4
+            </div>
+          </div>
+          <button>
+            View more News
+          </button>
+        </div>
+      </div>
     </>
   )
 }
